af8091d8d3812583cce768cfc003998fd6ce40b5
DylanMR08 <dylanmejiaruiz8@gmail.com>
2024-04-03 18:01:56 -0600

    Hola

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
+let url = window.location.href;
+let swLoc = "semana4.github.io/service-worker.js";
+
 if(navigator.serviceWorker){
+    if(url.includes("localhost")){
+        swLoc='/service-worker.js';
+    }
     navigator.serviceWorker.register('/service-worker.js')
         .then(reg=>{
             // setTimeout(()=>{
